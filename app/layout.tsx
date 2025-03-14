0e85c46fd9141a4ef93824c79756a042710e563a
kaushik-2318 <kaushikverma321@gmail.com>
2025-03-14 11:28:14 +0530

    Navbar Added

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,19 @@
 import type { Metadata } from "next";
 import { Source_Sans_3 as FontSans } from "next/font/google";
 import "./globals.css";
+import Header from "@/components/common/header";
+import Footer from "@/components/common/footer";
 
 const fontSans = FontSans({
   variable: "--font-sans",
   subsets: ["latin"],
-  weight: ['200', '300', '400', '500', '600', '700', '800', '900'],
+  weight: ["200", "300", "400", "500", "600", "700", "800", "900"],
 });
 
 export const metadata: Metadata = {
   title: "Sommaire - AI Powered PDF Summarization",
-  description: "Save hours of reading time with AI powered PDF summarization. Transform lengthy PDFs into clear, accurate summaries in second with our advanced AI Technology.",
+  description:
+    "Save hours of reading time with AI powered PDF summarization. Transform lengthy PDFs into clear, accurate summaries in second with our advanced AI Technology.",
 };
 
 export default function RootLayout({
@@ -20,8 +23,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${fontSans.variable} font-sans antialiased`}  >
-        {children}
+      <body className={`${fontSans.variable} font-sans antialiased`}>
+        <div className="relative flex min-h-screen flex-col">
+          <Header />
+          <main className="flex-1">{children}</main>
+          <Footer />
+        </div>
       </body>
     </html>
   );
---
+++ components/common/footer.tsx
@@ -0,0 +1,3 @@
+export default function Footer() {
+  return <div className="bg-gray-50 py-12">footer</div>;
+}
