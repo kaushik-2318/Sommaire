2b27ae74c290b79c01fadee137a87af8ef9a1e73
kaushik-2318 <kaushikverma321@gmail.com>
2025-03-15 14:09:11 +0530

    Clerk Added for Authentication

--- components/common/header.tsx
+++ components/common/header.tsx
@@ -1,9 +1,11 @@
+'use client'
+
 import { FileText } from "lucide-react";
-import { Button } from "../ui/button";
 import NavLink from "./nav-link";
+import { UserButton } from "@clerk/nextjs";
+import { SignedIn, SignedOut } from "@clerk/clerk-react";
 
 export default function Header() {
-  const isLoggedIn = false;
   return (
     <nav className="container flex items-center justify-between py-4 lg:px-8 px-2 mx-auto">
       <div className="flex lg:flex-1">
@@ -16,25 +18,26 @@ export default function Header() {
       </div>
 
       <div className="flex lg:justify-center gap-4 lg:gap-12 lg:items-center">
-        {isLoggedIn ? (
+        <NavLink href="/#pricing">Pricing</NavLink>
+        <SignedIn>
           <NavLink href="/dashboard">Your Summaries</NavLink>
-        ) : (
-          <NavLink href="/#pricing">Pricing</NavLink>
-        )}
+        </SignedIn>
       </div>
 
       <div className="flex lg:justify-end lg:flex-1">
-        {isLoggedIn ? (
+        <SignedIn>
           <div className="flex gap-2 items-center">
             <NavLink href="/upload">Upload a PDF</NavLink>
             <div>Pro</div>
-            <Button>User</Button>
+            <SignedIn>
+              <UserButton />
+            </SignedIn>
           </div>
-        ) : (
-          <div>
-            <NavLink href="/sign-in">Sign In</NavLink>
-          </div>
-        )}
+        </SignedIn>
+
+        <SignedOut>
+          <NavLink href="/sign-in">Sign In</NavLink>
+        </SignedOut>
       </div>
     </nav>
   );
---
+++ components/upload/upload-form.tsx
@@ -0,0 +1,4 @@
+
+export default function UploadForm() {
+  return <div></div>;
+}
